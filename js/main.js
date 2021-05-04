02bc5a889ef57792aea02055c817000cae3a9693
Thomas <thomas.auffroy@gmail.com>
2021-05-04 15:39:30 +0200

    Add condition when window is resizing

--- js/main.js
+++ js/main.js
@@ -6,7 +6,14 @@ const str_pgv = pgv.innerText;
 var price = document.querySelector(".price strong");
 const str_price = price.innerText;
 
+var discount = document.querySelector("#discount");
+const str_discount = discount.innerText;
+
 var swt = document.querySelector(".switch input");
+
+
+
+var pair;
 
 slider.addEventListener("mousedown", function(event){
     slider.addEventListener("mousemove", update, false);
@@ -19,13 +26,15 @@ slider.addEventListener("mouseup", function(event){
 
 swt.addEventListener("click",prc,false);
 
+window.addEventListener("resize",reportWindowSize,false);
+
 prc()
 
 function prc(){
     if (swt.checked){
-        pair = {"10k" : 8*0.25, "50k" : 12*0.25, "100k" : 16*0.25, "500k" : 24*0.25, "1m" : 36*0.25};
+       pair = {"10k" : 8*0.75, "50k" : 12*0.75, "100k" : 16*0.75, "500k" : 24*0.75, "1m" : 36*0.75};
     }else{
-        pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
+       pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
     }
     update();
 }
@@ -35,3 +44,9 @@ function update(){
     price.innerText = str_price + Object.values(pair)[slider.value-1].toFixed(2).toString();
 }
     
+function reportWindowSize(){
+    if (window.innerWidth < 700)
+        discount.innerText = "-25%"
+    else
+        discount.innerText = str_discount
+}
