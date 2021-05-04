0646ab9bf816fb502246cbb3d42ba65f5eb153b2
Thomas <thomas.auffroy@gmail.com>
2021-05-04 14:06:56 +0200

    Create all functions to link slider to HTML

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-var test = document.querySelector("input");
+var slider = document.querySelector("input");
+
 var pgv = document.querySelector(".person-touched");
-var pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
+const str_pgv = pgv.innerText;
+
+var price = document.querySelector(".price strong");
+const str_price = price.innerText;
+
+var swt = document.querySelector(".switch input");
+
+slider.addEventListener("mousedown", function(event){
+    slider.addEventListener("mousemove", update, false);
+},false);
 
 
-// En théorie faire une substr ou équivalent, pour par réécrire "Pageviews"
-pgv.innerText = Object.keys(pair)[test.value-1] + " Pageviews";
+slider.addEventListener("mouseup", function(event){
+    slider.removeEventListener("mousemove", update, false);
+},false);  
 
+swt.addEventListener("click",prc,false);
 
-test.addEventListener('click', function(event){alert('Element clicked through funciton!')});
+prc()
 
-test.value;
+function prc(){
+    if (swt.checked){
+        pair = {"10k" : 8*0.25, "50k" : 12*0.25, "100k" : 16*0.25, "500k" : 24*0.25, "1m" : 36*0.25};
+    }else{
+        pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
+    }
+    update();
+}
 
-Object.keys(pair)[0]
+function update(){
+    pgv.innerText = Object.keys(pair)[slider.value-1].concat(" ",str_pgv);
+    price.innerText = str_price + Object.values(pair)[slider.value-1].toFixed(2).toString();
+}
+    
