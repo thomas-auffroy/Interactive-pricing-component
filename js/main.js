44a992cb4e6e62c85b2539161a3f7c967ab2e2a9
Thomas <thomas.auffroy@gmail.com>
2021-05-06 12:02:14 +0200

    Add comments and adjust slider Event

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var slider = document.querySelector("input");
+/* Init variables */
 
 var pgv = document.querySelector(".person-touched");
-const str_pgv = pgv.innerText;
+const str_pgv = pgv.innerText; 
 
 var price = document.querySelector(".price strong");
 const str_price = price.innerText;
@@ -11,34 +11,31 @@ const str_discount = discount.innerText;
 
 var swt = document.querySelector(".switch input");
 
+var slider = document.querySelector("input");
+
 var pair, bgOn, timeoutId;
 
-slider.addEventListener("mousedown", function(event){
-    slider.addEventListener("mousemove", update, false);
-},false);
+/* Add EventListener */
+slider.addEventListener("input",update,false); // Trigerred on change value
+swt.addEventListener("click",prc,false); // Trigerred on click
+window.addEventListener("resize",reportWindowSize,false); // Trigerred on resizing
 
-slider.addEventListener("mouseup",function(event){
-    setTimeout(function(){slider.removeEventListener("mousemove", update, false);},100);
-},false);  
-
-swt.addEventListener("click",prc,false);
-
-window.addEventListener("resize",reportWindowSize,false);
-
-prc();
+prc(); // Call once to check the state of the switch
 
 function prc(){
-    if (swt.checked){
+    pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
+    /* If the switch is toggled then the reduction is applied */
+    if (swt.checked)
        pair = {"10k" : 8*0.75, "50k" : 12*0.75, "100k" : 16*0.75, "500k" : 24*0.75, "1m" : 36*0.75};
-    }else{
-       pair = {"10k" : 8, "50k" : 12, "100k" : 16, "500k" : 24, "1m" : 36};
-    }
-    update();
+    update(); // Call update
 }
 
 function update(){
+    /* Match the slider value with the number of views and price */
     pgv.innerText = Object.keys(pair)[slider.value-1].concat(" ",str_pgv);
     price.innerText = str_price + Object.values(pair)[slider.value-1].toFixed(2).toString();
+    
+    /* Update background color */
     bgOn = (slider.value - 1)* 25;
     bgOn = bgOn.toString()+"%";
     slider.style.background = "linear-gradient(to right, var(--strong-cyan) "+ bgOn +
@@ -46,6 +43,7 @@ function update(){
 }
     
 function reportWindowSize(){
+    /* With mobile size, change the text of the discount box*/
     if (window.innerWidth < 700)
         discount.innerText = "-25%";
     else
